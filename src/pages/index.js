f8da9494598a9a8bd97f6fe0941e97260f3f1e2a
[your <alessandrofigo24@gmail.com>
2022-06-04 22:31:38 +0700

    tailwind setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -14,7 +14,6 @@ export default function Home() {
         <h1 className={styles.title}>
           You are a little too early
         </h1>
-
         <p className={styles.description}>
           It will be worth the wait
         </p>
