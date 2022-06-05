37c445c9912af4c826cbe7ec7fa167136212bad1
[your <alessandrofigo24@gmail.com>
2022-06-05 20:24:45 +0700

    fixed button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Link from 'next/link'
+
 import styles from '../../styles/Home.module.css'
 
 export default function Home() {
@@ -10,13 +12,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
       <main className={styles.main}>
-        <h1 className="text-5xl font-mono">
+        <h1 className="text-5xl font-mono text-center">
           You are a little too early
         </h1>
-        <p className="mt-4">
-          it will be worth the wait
-        </p>
+        <Link href="">
+          <div className="mt-4">
+            <div class="relative group overflow-hidden px-6 h-12 rounded-full flex space-x-2 items-center bg-gradient-to-r from-pink-500 to-purple-500 hover:to-purple-600 hover:cursor-pointer">
+              <span class="relative text-sm text-white font-bold font-mono">Get Started</span>
+              <div class="flex items-center -space-x-3 translate-x-3">
+                <div class="w-2.5 h-[1.6px] rounded bg-white origin-left scale-x-0 transition duration-300 group-hover:scale-x-100"></div>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 stroke-white -translate-x-2 transition duration-300 group-hover:translate-x-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </Link>
       </main>
     </div>
   )
