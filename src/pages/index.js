ce44dd9e850c130bfbe18273b649d4139b5b75b3
[your <alessandrofigo24@gmail.com>
2022-06-06 00:27:51 +0700

    minor fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import styles from '../../styles/Home.module.css'
 
 export default function Home() {
+
   return (
     <div className={styles.container}>
       <Head>
@@ -19,11 +20,11 @@ export default function Home() {
         </h1>
         <Link href="">
           <div className="mt-4">
-            <div class="relative group overflow-hidden px-6 h-12 rounded-full flex space-x-2 items-center bg-gradient-to-r from-pink-500 to-purple-500 hover:to-purple-600 hover:cursor-pointer">
-              <span class="relative text-sm text-white font-bold font-mono">Get Started</span>
-              <div class="flex items-center -space-x-3 translate-x-3">
-                <div class="w-2.5 h-[1.6px] rounded bg-white origin-left scale-x-0 transition duration-300 group-hover:scale-x-100"></div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 stroke-white -translate-x-2 transition duration-300 group-hover:translate-x-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <div className="relative group overflow-hidden px-6 h-12 rounded-full flex space-x-2 items-center bg-gradient-to-r from-pink-500 to-purple-500 hover:to-purple-600 hover:cursor-pointer">
+              <span className="relative text-sm text-white font-bold font-mono">Get Started</span>
+              <div className="flex items-center -space-x-3 translate-x-3">
+                <div className="w-2.5 h-[1.6px] rounded bg-white origin-left scale-x-0 transition duration-300 group-hover:scale-x-100"></div>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 stroke-white -translate-x-2 transition duration-300 group-hover:translate-x-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
                 </svg>
               </div>
