586e93771d6ec9f531a4faf7ed1d51421829c09c
[your <alessandrofigo24@gmail.com>
2022-06-06 00:29:57 +0700

    minor change

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,8 +24,8 @@ export default function Home() {
               <span className="relative text-sm text-white font-bold font-mono">Get Started</span>
               <div className="flex items-center -space-x-3 translate-x-3">
                 <div className="w-2.5 h-[1.6px] rounded bg-white origin-left scale-x-0 transition duration-300 group-hover:scale-x-100"></div>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 stroke-white -translate-x-2 transition duration-300 group-hover:translate-x-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 stroke-white -translate-x-2 transition duration-300 group-hover:translate-x-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
                 </svg>
               </div>
             </div>
